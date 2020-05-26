09105d6ed8b1c3270e0bf794ab9cc16f5de7e855
vspv <seva--seva@ukr.net>
2020-05-27 01:14:57 +0300

    Updates

---
+++ precache-manifest.b87fdfe4380402f9a8998a9f4ccecfae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb8ef4d91e2a9a0f0e3e85453c15aae",
+    "url": "/corona_statistics/index.html"
+  },
+  {
+    "revision": "0ea91da64290e2d36055",
+    "url": "/corona_statistics/static/css/main.c1c755c1.chunk.css"
+  },
+  {
+    "revision": "d109427f5e17acf33348",
+    "url": "/corona_statistics/static/js/2.a15c7142.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_statistics/static/js/2.a15c7142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea91da64290e2d36055",
+    "url": "/corona_statistics/static/js/main.75edc41a.chunk.js"
+  },
+  {
+    "revision": "06452d409a090665f33a",
+    "url": "/corona_statistics/static/js/runtime-main.4dfc704d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_statistics/static/media/image.d7265326.png"
+  }
+]);
